6ddf9738ea57eadb410e2497d89faa09b7e2a655
Bhumi Patel <bhumiv1999@yahoo.com>
2020-06-29 12:22:51 -0700

    Initial Commit

---
+++ WebViewApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebViewApplication
+//
+//  Created by Bhumi Patel on 6/29/20.
+//  Copyright © 2020 Bhumi Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
